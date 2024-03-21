449697b1c3eebcec806256751b591783cb7a65ab
krimamadhikarmi <madhikrima20@gmail.com>
2024-03-21 10:47:52 +0545

    Instapost

---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+
+Rails.application.routes.draw do
+  # root 'home#index'
+  root 'posts#index'
+
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+
+  resources :posts
+  
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  delete 'logout', to: 'sessions#destroy', as: 'logout'
+end
